ea71bcb3054cf8481a2c3f9157db93fabb131502
Wilberto Pacheco Batista <wilbertopachecob@gmail.com>
2021-10-30 21:37:42 -0500

    added like component and clickhandler function

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import { getMovies, deleteMovie } from "../services/fakeMovieService";
+import Like from "./common/like.jsx";
 
 class Movies extends Component {
   state = {
@@ -10,6 +11,15 @@ class Movies extends Component {
     deleteMovie(id);
     this.setState({
       movies: getMovies(),
+    });
+  }
+
+  updateLike(movie) {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index].liked = !movies[index].liked;
+    this.setState({
+      movies,
     });
   }
 
@@ -26,6 +36,7 @@ class Movies extends Component {
               <th scope="col">Stock</th>
               <th scope="col">Rate</th>
               <th scope="col"></th>
+              <th scope="col"></th>
             </tr>
           </thead>
           <tbody>
@@ -35,6 +46,12 @@ class Movies extends Component {
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    isActive={movie.liked}
+                    onChange={() => this.updateLike(movie)}
+                  />
+                </td>
                 <td>
                   <button
                     className="btn btn-danger btn-sm"
