8c2eddd3a416091a946b8eae8020dccb9f8d8d97
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-11-12 14:09:32 -0600

    created routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import Movies from "./components/movies.jsx";
+import Navbar from "./components/navbar.jsx";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Rentals from './components/rentals';
+import Customers from './components/customers';
+import NotFound from './components/not-found';
+
 
 function App() {
   return (
     <main className="container">
-      <Movies></Movies>
+      <Navbar />
+      <Switch>
+        <Route path="/movies" component={Movies} />
+        <Route path="/rentals" component={Rentals} />
+        <Route path="/customers" component={Customers} />
+        <Route path="/not-found" component={NotFound} />
+        <Redirect from="/" exact to="/movies"/>
+        <Redirect to="/not-found" />
+      </Switch>
     </main>
   );
 }
