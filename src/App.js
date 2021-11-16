a8faaea5fb537abd1f75de3e7c75e869fabe11cb
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-11-15 22:28:42 -0600

    Added new route to MovieForm

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import Movies from "./components/movies.jsx";
 import Navbar from "./components/navbar.jsx";
 import { Switch, Route, Redirect } from "react-router-dom";
-import Rentals from './components/rentals';
-import Customers from './components/customers';
-import NotFound from './components/not-found';
-
+import Rentals from "./components/rentals";
+import Customers from "./components/customers";
+import NotFound from "./components/not-found";
+import MovieForm from "./components/movieForm";
+import React from "react";
 
 function App() {
   return (
-    <main className="container">
+    <React.Fragment>
       <Navbar />
-      <Switch>
-        <Route path="/movies" component={Movies} />
-        <Route path="/rentals" component={Rentals} />
-        <Route path="/customers" component={Customers} />
-        <Route path="/not-found" component={NotFound} />
-        <Redirect from="/" exact to="/movies"/>
-        <Redirect to="/not-found" />
-      </Switch>
-    </main>
+      <main className="container mt-3">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
