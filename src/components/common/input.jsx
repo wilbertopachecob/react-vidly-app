a95dad7e8db62985bd57092056e3be76ce3f8d6e
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-11-16 17:13:40 -0600

    created input component

---
+++ src/components/common/input.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+Input.propTypes = {
+  name: PropTypes.string,
+  label: PropTypes.string,
+  error: PropTypes.string,
+  onChange: PropTypes.func,
+  value: PropTypes.string,
+  type: PropTypes.string,
+};
+
+function Input({ name, label, error, onChange, value, type }) {
+  return (
+    <div className="mb-3">
+      <label htmlFor={name} className="form-label">
+        {label}
+      </label>
+      <input
+        value={value}
+        onChange={onChange}
+        type={type}
+        className="form-control"
+        id={name}
+        name={name}
+        aria-describedby={name}
+      />
+      {error && <div className="alert alert-danger mt-1">{error}</div>}
+    </div>
+  );
+}
+
+export default Input;
