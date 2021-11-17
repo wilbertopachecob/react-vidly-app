5e8bcae659d64bc0fd52a40b12217c996538cbb2
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-11-17 16:25:38 -0600

    added form to component

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,17 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Joi from "joi-browser";
+import Form from "./utils/forms-helper.js";
+import { getMovie, saveMovie } from "../services/fakeMovieService";
+import { getGenres } from "../services/fakeGenreService";
 
-function MovieForm({match, history}) {
-  function saveMovie() {
-    history.push("/movies");
+const initialData = {
+  title: "",
+  dailyRentalRate: "",
+  genreId: "",
+  numberInStock: "",
+};
+
+function MovieForm({ match, history }) {
+  const [data, setData] = useState(initialData);
+  const [errors, setErrors] = useState({});
+  const genres = getGenres();
+  const schema = {
+    _id: Joi.string().optional(),
+    title: Joi.string().required().label("Title"),
+    genreId: Joi.string().required().label("Genre"),
+    numberInStock: Joi.number()
+      .min(0)
+      .max(100)
+      .required()
+      .label("Number in stock"),
+    dailyRentalRate: Joi.number().min(0).max(10).required().label("Rate"),
+  };
+
+  const form = Form(data, setData, errors, setErrors, schema);
+
+  useEffect(() => {
+    const movie = getMovie(match.params.id);
+    if (!movie) {
+      return history.replace("/not-found");
+    }
+    const data = {
+      _id: movie._id,
+      title: movie.title,
+      dailyRentalRate: movie.dailyRentalRate,
+      genreId: movie.genre._id,
+      numberInStock: movie.numberInStock,
+    };
+    setData(data);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [match.params]);
+
+  function goTo(path) {
+    history.push(path);
+  }
+
+  function handleSubmit(e) {
+    const isValid = form.handleSubmit(e);
+    if (isValid === false) {
+      return;
+    }
+    saveMovie(data);
+    setData(initialData);
+    goTo("/movies");
   }
 
   return (
-    <div>
-      <h1>Movie - ID: {match.params.id} </h1>
-      <button className="btn btn-primary" onClick={saveMovie}>
-        Save
-      </button>
-    </div>
+    <form onSubmit={(e) => handleSubmit(e)}>
+      <h1>Movie Form</h1>
+      {form.renderInput("title", "Title")}
+      {form.renderSelect(genres, "genreId", "Genre")}
+      {form.renderInput("numberInStock", "Number In Stock", "number")}
+      {form.renderInput("dailyRentalRate", "Rate", "number")}
+      {form.renderSubmitButton()}
+    </form>
   );
 }
 
