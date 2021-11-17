80207628613575bd14946bdb9257a9038ef49882
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-11-17 16:26:26 -0600

    created new movies component to add a movie

---
+++ src/components/newMovie.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import Joi from "joi-browser";
+import Form from "./utils/forms-helper.js";
+import { getGenres } from "../services/fakeGenreService";
+import { saveMovie } from "../services/fakeMovieService";
+
+const initialData = {
+  title: "",
+  dailyRentalRate: "",
+  genreId: "",
+  numberInStock: "",
+};
+
+function NewMovie() {
+  const [data, setData] = useState(initialData);
+  const [errors, setErrors] = useState({});
+  const genres = getGenres();
+  const schema = {
+    title: Joi.string().required().label("Title"),
+    genreId: Joi.string().required().label("Genre"),
+    numberInStock: Joi.number()
+      .min(0)
+      .max(100)
+      .required()
+      .label("Number in stock"),
+    dailyRentalRate: Joi.number().min(0).max(10).required().label("Rate"),
+  };
+
+  const form = Form(data, setData, errors, setErrors, schema);
+
+  function handleSubmit(e) {
+    const isValid = form.handleSubmit(e);
+    if (isValid === false) {
+      return;
+    }
+    saveMovie(data);
+    setData(initialData);
+  }
+
+  return (
+    <form onSubmit={(e) => handleSubmit(e)}>
+      <h1>Movie Form</h1>
+      {form.renderInput("title", "Title")}
+      {form.renderSelect(genres, "genreId", "Genre")}
+      {form.renderInput("numberInStock", "Number In Stock", "number")}
+      {form.renderInput("dailyRentalRate", "Rate", "number")}
+      {form.renderSubmitButton()}
+    </form>
+  );
+}
+
+export default NewMovie;
