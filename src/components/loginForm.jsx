df2e186fa2690c8924a0c251c2ec908625b6a1da
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-11-24 22:43:30 -0600

    validating login with server

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import Joi from "joi-browser";
 import Form from "./utils/forms-helper.js";
+import { login } from "../services/authService";
 
-function LoginForm() {
+function LoginForm({ history }) {
   const [data, setData] = useState({ username: "", password: "" });
   const [errors, setErrors] = useState({});
   const schema = {
@@ -12,11 +13,34 @@ function LoginForm() {
 
   const form = Form(data, setData, errors, setErrors, schema);
 
+  function storeToken(token) {
+    localStorage.setItem("token", token);
+  }
+
+  async function handleSubmit(e) {
+    const isValid = form.handleSubmit(e);
+    if (isValid === false) {
+      return;
+    }
+
+    try {
+      const { data: token } = await login(data.username, data.password);
+      storeToken(token);
+      history.push("/");
+    } catch (error) {
+      if (error.response && error.response.status === 400) {
+        const errorsCopy = { ...errors };
+        errorsCopy.username = error.response.data;
+        setErrors(errorsCopy);
+      }
+    }
+  }
+
   return (
-    <form onSubmit={(e) => form.handleSubmit(e)}>
+    <form onSubmit={(e) => handleSubmit(e)}>
       <h1>Login</h1>
-      {form.renderInput('username', 'Username')}
-      {form.renderInput('password', 'Password', 'password')}
+      {form.renderInput("username", "Username")}
+      {form.renderInput("password", "Password", "password")}
       {form.renderSubmitButton()}
     </form>
   );
