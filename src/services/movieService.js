ac589eb1a26fe41b2775da3e0236a3713484bab4
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-11-24 19:32:26 -0600

    added saveMovie and getMovie functions

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -2,9 +2,17 @@ import { ENV } from "../const/env";
 import httpService from "../services/httpService";
 
 export function getMovies() {
-    return httpService.get(`${ENV.API_HOST}/movies`);
+  return httpService.get(`${ENV.API_HOST}/movies`);
 }
 
 export function deleteMovie(id) {
-    return httpService.delete(`${ENV.API_HOST}/movies/${id}`);
+  return httpService.delete(`${ENV.API_HOST}/movies/${id}`);
+}
+
+export function getMovie(id) {
+  return httpService.get(`${ENV.API_HOST}/movies/${id}`);
+}
+
+export function saveMovie(movie) {
+    return httpService.post(`${ENV.API_HOST}/movies`, movie);
 }
