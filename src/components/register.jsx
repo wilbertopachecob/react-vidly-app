fc3b5b0e48f66abc8e44342ff3d0aa86dcd871eb
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-11-24 21:04:44 -0600

    handle registration with server

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import Joi from "joi-browser";
 import Form from "./utils/forms-helper.js";
+import { register } from "../services/userService";
+import { toast } from 'react-toastify';
+
+const initialData = { username: "", password: "", name: "" }
 
 function RegisterForm() {
-  const [data, setData] = useState({ username: "", password: "", name: "" });
+  const [data, setData] = useState(initialData);
   const [errors, setErrors] = useState({});
   const schema = {
     username: Joi.string().required().label("Email Address"),
@@ -13,8 +17,27 @@ function RegisterForm() {
 
   const form = Form(data, setData, errors, setErrors, schema);
 
+  async function handleSubmit(e) {
+    const isValid = form.handleSubmit(e);
+    if (isValid === false) {
+      return;
+    }
+
+    try {
+      await register(data);
+      setData(initialData);
+      toast.success('User registered successfully');
+    } catch (error) {
+      if (error.response && error.response.status === 400) {
+        const errorsCopy = {...errors}
+        errorsCopy.username = error.response.data;
+        setErrors(errorsCopy);
+      }
+    }
+  }
+
   return (
-    <form onSubmit={(e) => form.handleSubmit(e)}>
+    <form onSubmit={(e) => handleSubmit(e)}>
       <h1>Register</h1>
       {form.renderInput("username", "Username")}
       {form.renderInput("password", "Password", "password")}
